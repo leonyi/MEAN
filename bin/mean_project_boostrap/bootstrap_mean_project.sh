ee3e73cb25e0bb0f578c44353bd2176ac98ac93c
Yanet Leon <yanet.leon@gmail.com>
2018-02-20 21:16:30 -0800

    bootrap_mean_project.sh: adding httpservice setup and fixing typs on function to transfer server files.

--- bin/mean_project_boostrap/bootstrap_mean_project.sh
+++ bin/mean_project_boostrap/bootstrap_mean_project.sh
@@ -5,11 +5,9 @@
 # Name: bootstrap_mean_project.sh 
 
 # Todo: Add args to setup the directory based on argument passed: n for node, e for express, etc.
-# The express projects now are using Mongoose, they are now located under MeaN/Mongoose.
-# PROJECT_HOME="/Users/yleon/Dev/MeaN/Mongoose"
 PROJECT_HOME="/Users/yleon/Dev/Angular"
-SERVER_SOURCE="/Users/yleon/Dev/MeaN/bin/modularized_bootstrap_with_angular/server"
-CLIENT_SOURCE="/Users/yleon/Dev/MeaN/bin/modularized_bootstrap_with_angular/angular"
+SERVER_SOURCE="/Users/yleon/Dev/MeaN/bin/mean_project_boostrap/server"
+CLIENT_SOURCE="/Users/yleon/Dev/MeaN/bin/mean_project_boostrap/angular"
 
 
 decorate () {
@@ -28,6 +26,13 @@ setup_server_dir_structure () {
 
  	decorate "Creating directory:"
 	log rsync -avh --stats --progress $SERVER_SOURCE $PROJECT_HOME/$project_name/
+}
+
+transfer_app_src_files () {
+	log cd $PROJECT_HOME/$project_name/client/src/app/ && mv app.module.ts was_app.module.ts && mv app.component.ts was_app.component.ts
+	log cp $CLIENT_SOURCE/app.module.ts     $PROJECT_HOME/$project_name/client/src/app/
+	log cp $CLIENT_SOURCE/app.component.ts  $PROJECT_HOME/$project_name/client/src/app/
+	log cp $CLIENT_SOURCE/http.service.ts   $PROJECT_HOME/$project_name/client/src/app/
 }
 
 
@@ -57,11 +62,29 @@ else
 	log cd $PROJECT_HOME/$project_name/server && npm install mongoose-validator -S
 
 	decorate "5. Setting up the client (angular) directory structore under  $PROJECT_HOME/$project_name"
-	log cd $PROJECT_HOME/$project_name/ && ng new client
+	log cd $PROJECT_HOME/$project_name/ && ng new client --routing
 
-	decorate "7. Transferring indext.html with mini boostrap setup under  $PROJECT_HOME/$project_name/client/src"
-	log mv $PROJECT_HOME/$project_name/client/src/index.html $PROJECT_HOME/$project_name/client/src/was_index.html
-	log cp $CLIENT_SOURCE/yeti_bootstrap_index.html $PROJECT_HOME/$project_name/client/src/index.html
+	# yleon: Leaving this out, at the moment, while I play with bootstrap installed from npm.
+	#decorate "6. Transferring indext.html with mini boostrap setup under  $PROJECT_HOME/$project_name/client/src"
+	#log mv $PROJECT_HOME/$project_name/client/src/index.html $PROJECT_HOME/$project_name/client/src/was_index.html
+	#log cp $CLIENT_SOURCE/yeti_bootstrap_index.html $PROJECT_HOME/$project_name/client/src/index.html
+
+	decorate "7. Generating the http service for our project under $PROJECT_HOME/$project_name/client"
+	log cd $PROJECT_HOME/$project_name/client && ng generate service http --no-spec && mv http.service.ts  was_http.service.ts 
+
+	decorate "8. Transferring app src files to $PROJECT_HOME/$project_name/client/src/app/"
+	transfer_app_src_files
+
+	decorate "9. Setting up boostrap under $PROJECT_HOME/$project_name/client/"
+	log cd $PROJECT_HOME/$project_name/client && npm install --save bootstrap@3
+
+	decorate "Remebmer to update: $PROJECT_HOME/$project_name/client/.angualar.cli with the following:"
+	echo '
+	    "styles": [
+            "../node_modules/bootstrap/dist/css/bootstrap.min.css”,
+            "styles.css"
+            ],
+	'	
 
 	decorate "MEAN project ready at $PROJECT_HOME/$project_name!"
 	
